76d2719902c8db981fe36d15eef85e0921de1fbf
Tom Wang <tomwang@MacBook-Pro.local>
2023-07-17 12:10:43 -0400

    test multi ping sent in same connection

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -14,6 +15,7 @@ func main() {
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
+
 	}
 	conn, err := l.Accept()
 	if err != nil {
@@ -21,16 +23,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	// // incoming request, make buffer size to 1024 byte
-	// buffer := make([]byte, 1024)
-	// data, err := conn.Read(buffer)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	buffer := make([]byte, 1024)
+	// the loop handles data sent through the same connection opened above
+	for {
+		// sends response so long as data is sent to socket, buffer contains data
+		_, err = conn.Read(buffer)
+		if err != nil {
+			// had to put this within outter err checking block
+			// to handle EOF(no more data sent) for some reason
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println("Error reading data sent through connection", err.Error())
+				os.Exit(1)
+			}
+		}
 
-	// this converts strings to bytes as response and sends through socket
-	conn.Write([]byte("+PONG\r\n"))
-
-	conn.Close()
-
+		// this converts strings to bytes as response and sends through socket
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error sending response", err.Error())
+			os.Exit(1)
+		}
+	}
 }
